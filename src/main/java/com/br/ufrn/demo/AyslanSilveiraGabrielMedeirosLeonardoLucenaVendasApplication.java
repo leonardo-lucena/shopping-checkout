d9741dddc99d2de7a18ab06c03f04bfba4c7f7e9
Leonardo Lucena <leonardolucena.cc@gmail.com>
2023-11-29 16:19:09 -0300

    :tada: Start project

---
+++ src/main/java/com/br/ufrn/demo/AyslanSilveiraGabrielMedeirosLeonardoLucenaVendasApplication.java
@@ -0,0 +1,13 @@
+package com.br.ufrn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyslanSilveiraGabrielMedeirosLeonardoLucenaVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyslanSilveiraGabrielMedeirosLeonardoLucenaVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ufrn/demo/AyslanSilveiraGabrielMedeirosLeonardoLucenaVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ufrn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyslanSilveiraGabrielMedeirosLeonardoLucenaVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
