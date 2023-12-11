93d7465a6404385e6998fb14b0bfdd813cb0c65c
Ayslan Câmara de Moura Silveira <ayslan.silveira@jfpe.jus.br>
2023-12-11 18:52:11 -0300

    suite de testes

--- src/test/java/com/br/ufrn/demo/ShoppingCartServiceTestes.java
+++ src/test/java/com/br/ufrn/demo/ShoppingCartServiceTestes.java
@@ -2,12 +2,17 @@ package com.br.ufrn.demo;
 
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
+import com.br.ufrn.demo.ShoppingCartServiceTesteUmProdutoApplicationTests;
+import com.br.ufrn.demo.ShoppingCartServiceTesteCincoProdutosApplicationTests;
+import com.br.ufrn.demo.ShoppingCartServiceTesteCincoProdutosComRepeticaoApplicationTests;
+
+// Add the missing JUnit dependencies to your project's build configuration file
 
 @Suite
 @SelectClasses({
-        ShoppingCartServiceTesteUmProdutoApplicationTests.class,
-        ShoppingCartServiceTesteCincoProdutosApplicationTests.class,
-        ShoppingCartServiceTesteCincoProdutosComRepeticaoApplicationTests.class
+                ShoppingCartServiceTesteUmProdutoApplicationTests.class,
+                ShoppingCartServiceTesteCincoProdutosApplicationTests.class,
+                ShoppingCartServiceTesteCincoProdutosComRepeticaoApplicationTests.class
 })
 public class ShoppingCartServiceTestes {
 }
